79a21215fb829a56682fc7d6aa0e7b946848b64c
Ben Martin <ben@benmartinstudios.com.au>
2016-10-11 13:56:56 +1000

    Added visualization of Morley's Miracle.

---
+++ morleys_miracle.py
@@ -0,0 +1,180 @@
+import tkinter as tk
+import itertools
+import math
+from point import Point, Vector
+
+
+DEFAULT_WIDTH = 600
+DEFAULT_HEIGHT = 600
+
+COLOURS = ['gold', 'light sky blue', 'firebrick']
+ARC_COLOURS = COLOURS
+
+
+def euclidean_distance(a, b):
+    ax, ay = a
+    bx, by = b
+
+    return ((bx - ax) ** 2 + (by - ay) ** 2) ** 0.5
+
+
+
+class TriangleApp(tk.Frame):
+    def __init__(self, parent, *args, **kwargs):
+        super().__init__(parent, *args, **kwargs)
+
+        self._canvas = tk.Canvas(self, width=DEFAULT_WIDTH,
+                                 height=DEFAULT_HEIGHT)
+        self._canvas.pack(side=tk.TOP, expand=True, fill=tk.BOTH)
+
+        self.bind('<Configure>', self._resize)
+        self._canvas.bind('<Button-1>', self._click)
+        self._canvas.bind('<B1-Motion>', self._drag)
+
+        self._points = None
+
+    def _click(self, ev):
+        self._index, _ = min(enumerate(self._points),
+                             key=lambda pair: euclidean_distance(pair[1],
+                                                                 (ev.x, ev.y)))
+
+        #print("Closest point is {}".format(self._points[self._index]))
+
+        self.draw()
+
+    def _drag(self, ev):
+        self._points[self._index] = Point(ev.x, ev.y)
+
+        self.draw()
+
+    def draw(self):
+        self._canvas.delete(tk.ALL)
+        self._canvas.create_polygon(*(tuple(p) for p in self._points), fill='white')
+
+        # Draw equilaterals
+        for i, colour in enumerate(COLOURS):
+            p1, p2, orientator = [self._points[(i + j) % len(self._points)] for
+                                  j in range(3)]
+            p3 = self.generate_equilateral(p1, p2, orientator)
+
+            sprite = self._canvas.create_polygon(tuple(p1), tuple(p2), p3,
+                                                 fill=colour, outline='black')
+
+        # Draw arcs
+        arc_size = 0.3
+
+        trisections = []
+
+        for i, colour in enumerate(ARC_COLOURS):
+
+            p1, p2, p3 = [self._points[(i + j) % len(self._points)] for
+                          j in range(3)]
+
+            side1 = p3.distance(p1)
+            side2 = p3.distance(p2)
+            side = min(side1, side2)
+            r = side * arc_size
+
+            x, y = p3
+            v1 = p1 - p3
+            v2 = p2 - p3
+
+            a1 = math.atan2(*(v1)) - math.pi / 2
+            extent = v2.angle_between(v1)
+
+            d = v2.determinant(v1)
+            if d < 0:
+                extent = -extent
+
+            for i in range(1, 3):
+                trisections.append((p3, a1 + extent * i / 3))
+
+            self._canvas.create_arc(x - r, y - r, x + r, y + r,
+                                    start=math.degrees(a1),
+                                    extent=math.degrees(extent), fill=colour)
+
+        # Draw trisections of inner angles
+        inner_points = []
+
+        for i in range(3):
+            p1, theta1 = trisections[i]
+            p2, theta2 = trisections[i + 3]
+            colour = COLOURS[(2 - i) % 3]
+
+            v1, v2 = [Point.from_polar(200, -theta) for theta in (theta1, theta2)]
+
+            l1 = Vector(p1, v1)
+            l2 = Vector(p2, v2)
+
+            p3 = l1.intersects2d(l2)
+            inner_points.append(p3)
+            p3 = tuple(p3)
+
+            [self._canvas.create_line(p3, tuple(p), fill=colour) for p in (p1, p2)]
+
+
+        # Draw inner triangle
+        self._canvas.create_polygon(*(tuple(p) for p in inner_points), fill='white', outline='black')
+
+
+    def generate_equilateral(self, p1, p2, orientator, toward_orientator=False):
+        """
+        Draws an equilateral on p1->p2.
+
+        :param p1: Point 1 on the equilateral triangle.
+        :param p2: Point 2 on the equilateral triangle.
+        :param orientator: Orientation point on the equilateral triangle.
+        :param toward_orientator: If True, third point on equilateral is drawn
+                                  toward orientator point. Defaults to False.
+
+        :return: Point 3 on the equilateral triangle.
+        """
+
+        p1x, p1y = p1
+        p2x, p2y = p2
+
+        dx, dy = p2x - p1x, p2y - p1y
+
+        r = euclidean_distance((dx, dy), (0, 0))
+        theta = math.atan2(dy, dx)
+
+        candidates = []
+
+        for dtheta in (math.pi / 3, -math.pi / 3):
+            dx = r * math.cos(theta + dtheta)
+            dy = r * math.sin(theta + dtheta)
+
+            point = p1x + dx, p1y + dy
+
+            distance = euclidean_distance(point, orientator)
+
+            candidates.append((distance, point))
+
+        candidates.sort()
+        if toward_orientator:
+            candidates.reverse()
+        p3 = candidates[1][1]
+
+        return p3
+
+    def _resize(self, ev):
+        if self._points is None:
+            width = ev.width
+            height = ev.height
+            self._points = [Point(int(x), int(y)) for x, y in [
+                (226 / 600 * width, 177 / 600 * height),
+                (427 / 600 * width, 293 / 600 * height),
+                (154 / 600 * width, 316 / 600 * height)
+            ]]
+
+        self.draw()
+
+
+def main():
+    root = tk.Tk()
+    TriangleApp(root).pack(expand=True, fill=tk.BOTH)
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
---
+++ point.py
@@ -0,0 +1,189 @@
+from numbers import Number
+import math
+
+class Point:
+    """
+    Represents an n-dimensional point/vector.
+
+    Point is often treated as a vector, and has many relevant methods.
+
+    TODO: unary ops, normal/reflected arithmetic ops
+    """
+
+    __slots__ = ['_coords']
+
+    def __init__(self, *coords):
+        """
+        Constructs Point with dimensions equal to number of coordinates.
+        """
+        self._coords = tuple(coords)
+
+    def __repr__(self):
+        return "Point{!r}".format(self._coords)
+
+    def __iter__(self):
+        return iter(self._coords)
+
+    def __getitem__(self, item):
+        return self._coords[item]
+
+    def __len__(self):
+        return len(self._coords)
+
+    def __pos__(self):
+        raise NotImplementedError
+
+    def __neg__(self):
+        return Point(*(-x for x in self._coords))
+
+    def __abs__(self):
+        raise NotImplementedError
+    def __invert__(self):
+        raise NotImplementedError
+    def __round__(self, n):
+        raise NotImplementedError
+    def __floor__(self):
+        raise NotImplementedError
+    def __ceil__(self):
+        raise NotImplementedError
+    def __trunc__(self):
+        raise NotImplementedError
+
+    def __add__(self, other):
+        return Point(*(x + y for x, y in zip(self._coords, other._coords)))
+
+    def __sub__(self, other):
+        return Point(*(x - y for x, y in zip(self._coords, other._coords)))
+
+    def __mul__(self, other):
+        if isinstance(other, Number):
+            return Point(*(x * other for x in self._coords))
+        elif isinstance(other, Point):
+            return Point(*(x * y for x, y in zip(self._coords, other._coords)))
+        else:
+            raise TypeError("Unsupported type.")
+
+    # Multiplication is commutative
+    __rmul__ = __mul__
+    # def __rmul__(self, other):
+    #     return self.__mul__(other)
+
+    def __pow__(self, power, modulo=None):
+        return Point(*(x ** power for x in self._coords))
+
+    def distance(self, other):
+        return sum([(x - y) ** 2 for x, y in zip(self._coords, other._coords)]) ** 0.5
+
+    def angle_between(self, other):
+        """
+        Returns the angle between two vectors.
+
+        :param other: Other vector (instance of Point).
+        :return: Angle between self & other in radians.
+        """
+
+        return math.acos(self.dot(other)/(self.length() * other.length()))
+
+    def angle(self):
+        """
+        Returns the angle between this vector and positive x-axis.
+
+        :param other: Other vector (instance of Point).
+        :return: Angle between self & other in radians.
+        """
+        return self.angle_between(Point(0, 1))
+
+    def length(self):
+        """
+        Returns the length of this vector.
+
+        :return: float
+        """
+        return sum([x ** 2 for x in self._coords]) ** 0.5
+
+    def __float__(self):
+        """
+        :return: Length of vector.
+        """
+        return self.length()
+
+    def dot(self, other):
+        """
+        Returns the dot product of two vectors.
+
+        :param other:
+        :return:
+        """
+        return sum(self * other)
+
+    def determinant(self, other):
+        if len(self) != 2 or len(other) != 2:
+            raise ValueError("Cannot take determinant of non-2d vectors.")
+
+        x1, y1 = self._coords
+        x2, y2 = other._coords
+
+        return x1 * y2 - x2 * y1
+
+    @classmethod
+    def from_polar(cls, r, angle):
+        x = r * math.cos(angle)
+        y = r * math.sin(angle)
+
+        return Point(x, y)
+
+class Vector:
+    __slots__ = ['_point', '_direction']
+
+    def __init__(self, point, direction):
+        self._point = point
+        self._direction = direction
+
+    @classmethod
+    def from_direction(cls, direction):
+        point = Point(tuple(0 for i in range(len(direction))))
+        return Vector(point, direction)
+
+    def intersects2d(self, other):
+        m1, c1 = self.to_scalar()
+        m2, c2 = other.to_scalar()
+
+        x = (c1 - c2)/(m2 - m1)
+        y = m1 * x + c1
+
+        return Point(x, y)
+
+    def to_scalar(self):
+        x0, y0 = self._point
+        vx, vy = self._direction
+
+        m = vy/vx
+        c = y0 - m * x0
+
+        return m, c
+
+
+
+
+
+def main():
+    p1 = Point(3, 4)
+    p2 = Point(1,2)
+    print(sum((p1 - p2)**2) ** 0.5)
+    print(p1.distance(p2))
+    print(p1 ** 2)
+    print(tuple(p1))
+
+    print(p1 * 5)
+    print(5.5 * p1)
+
+    print(p1.length())
+
+    l1 = Vector(Point(1,3), (1,1))
+    l2 = Vector(Point(4,2), (-1,1))
+
+    print(l1.intersects2d(l2))
+
+if __name__ == "__main__":
+    main()
+
